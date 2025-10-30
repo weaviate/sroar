cc1b788e93a36f1e333833a62d2dcda672420bf7
Andrzej Liszka <andrzej.liszka@gmail.com>
2025-10-30 13:51:10 +0100

    benchmark mergepairs 0.0.13 update

--- benchmark_bitmaps_test.go
+++ benchmark_bitmaps_test.go
@@ -17,6 +17,11 @@ import (
 // Benchmark_MergePairs_CurrencyUSD_10-10          404           2926223 ns/op           24220 B/op        192 allocs/op
 // Benchmark_MergePairs_CurrencyUSD_10-10          404           2944520 ns/op           24274 B/op        192 allocs/op
 // Benchmark_MergePairs_CurrencyUSD_10-10          400           2964674 ns/op           24270 B/op        192 allocs/op
+//
+// 0.0.13 / cpu: Apple M1 Pro
+// Benchmark_MergePairs_CurrencyUSD_10-10               572           2110369 ns/op           16741 B/op        185 allocs/op
+// Benchmark_MergePairs_CurrencyUSD_10-10               571           2118648 ns/op           16360 B/op        185 allocs/op
+// Benchmark_MergePairs_CurrencyUSD_10-10               570           2106059 ns/op           16365 B/op        185 allocs/op
 func Benchmark_MergePairs_CurrencyUSD_10(b *testing.B) {
 	benchmarkMergeOrAndNot(b, "currency_usd", 10)
 }
@@ -27,6 +32,11 @@ func Benchmark_MergePairs_CurrencyUSD_10(b *testing.B) {
 // Benchmark_MergePairs_CurrencyUSD_1-10                230           5256425 ns/op           19450 B/op         21 allocs/op
 // Benchmark_MergePairs_CurrencyUSD_1-10                224           5261322 ns/op           19454 B/op         21 allocs/op
 // Benchmark_MergePairs_CurrencyUSD_1-10                224           5258985 ns/op           19386 B/op         21 allocs/op
+//
+// 0.0.13 / cpu: Apple M1 Pro
+// Benchmark_MergePairs_CurrencyUSD_1-10                225           5350589 ns/op            5299 B/op         11 allocs/op
+// Benchmark_MergePairs_CurrencyUSD_1-10                228           5306998 ns/op            5226 B/op         11 allocs/op
+// Benchmark_MergePairs_CurrencyUSD_1-10                230           5409507 ns/op            5216 B/op         11 allocs/op
 func Benchmark_MergePairs_CurrencyUSD_1(b *testing.B) {
 	benchmarkMergeOrAndNot(b, "currency_usd", 1)
 }
@@ -37,6 +47,11 @@ func Benchmark_MergePairs_CurrencyUSD_1(b *testing.B) {
 // Benchmark_MergePairs_CurrencyEUR_10-10          404           2950929 ns/op           24021 B/op        192 allocs/op
 // Benchmark_MergePairs_CurrencyEUR_10-10          406           3006315 ns/op           24059 B/op        192 allocs/op
 // Benchmark_MergePairs_CurrencyEUR_10-10          387           3029769 ns/op           24069 B/op        192 allocs/op
+//
+// 0.0.13 / cpu: Apple M1 Pro
+// Benchmark_MergePairs_CurrencyEUR_10-10               630           1906027 ns/op           52510 B/op        288 allocs/op
+// Benchmark_MergePairs_CurrencyEUR_10-10               632           1888014 ns/op           52171 B/op        288 allocs/op
+// Benchmark_MergePairs_CurrencyEUR_10-10               627           1906638 ns/op           52155 B/op        288 allocs/op
 func Benchmark_MergePairs_CurrencyEUR_10(b *testing.B) {
 	benchmarkMergeOrAndNot(b, "currency_eur", 10)
 }
@@ -47,6 +62,11 @@ func Benchmark_MergePairs_CurrencyEUR_10(b *testing.B) {
 // Benchmark_MergePairs_CurrencyEUR_1-10                248           4867205 ns/op           19344 B/op         20 allocs/op
 // Benchmark_MergePairs_CurrencyEUR_1-10                247           4752302 ns/op           19344 B/op         20 allocs/op
 // Benchmark_MergePairs_CurrencyEUR_1-10                250           4733349 ns/op           19344 B/op         20 allocs/op
+//
+// 0.0.13 / cpu: Apple M1 Pro
+// Benchmark_MergePairs_CurrencyEUR_1-10                234           5186423 ns/op            4935 B/op         10 allocs/op
+// Benchmark_MergePairs_CurrencyEUR_1-10                248           4779076 ns/op            4928 B/op         10 allocs/op
+// Benchmark_MergePairs_CurrencyEUR_1-10                249           4941889 ns/op            4928 B/op         10 allocs/op
 func Benchmark_MergePairs_CurrencyEUR_1(b *testing.B) {
 	benchmarkMergeOrAndNot(b, "currency_eur", 1)
 }
@@ -57,6 +77,11 @@ func Benchmark_MergePairs_CurrencyEUR_1(b *testing.B) {
 // Benchmark_MergePairs_RetailerWalmart_10-10             5652            205146 ns/op             512 B/op         12 allocs/op
 // Benchmark_MergePairs_RetailerWalmart_10-10             5912            196649 ns/op             523 B/op         12 allocs/op
 // Benchmark_MergePairs_RetailerWalmart_10-10             6054            195562 ns/op             518 B/op         12 allocs/op
+//
+// 0.0.13 / cpu: Apple M1 Pro
+// Benchmark_MergePairs_RetailerWalmart_10-10          5482            220376 ns/op             545 B/op         12 allocs/op
+// Benchmark_MergePairs_RetailerWalmart_10-10          5720            205585 ns/op             512 B/op         12 allocs/op
+// Benchmark_MergePairs_RetailerWalmart_10-10          5800            207235 ns/op             512 B/op         12 allocs/op
 func Benchmark_MergePairs_RetailerWalmart_10(b *testing.B) {
 	benchmarkMergeOrAndNot(b, "retailer_walmart", 10)
 }
@@ -67,6 +92,11 @@ func Benchmark_MergePairs_RetailerWalmart_10(b *testing.B) {
 // Benchmark_MergePairs_RetailerWalmart_1-10           2292            460649 ns/op              64 B/op          2 allocs/op
 // Benchmark_MergePairs_RetailerWalmart_1-10           2504            465026 ns/op              64 B/op          2 allocs/op
 // Benchmark_MergePairs_RetailerWalmart_1-10           2432            449451 ns/op              64 B/op          2 allocs/op
+//
+// 0.0.13 / cpu: Apple M1 Pro
+// Benchmark_MergePairs_RetailerWalmart_1-10           2538            448390 ns/op              64 B/op          2 allocs/op
+// Benchmark_MergePairs_RetailerWalmart_1-10           2640            446692 ns/op              64 B/op          2 allocs/op
+// Benchmark_MergePairs_RetailerWalmart_1-10           2625            446678 ns/op              64 B/op          2 allocs/op
 func Benchmark_MergePairs_RetailerWalmart_1(b *testing.B) {
 	benchmarkMergeOrAndNot(b, "retailer_walmart", 1)
 }
